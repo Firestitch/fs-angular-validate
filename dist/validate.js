fa4e271651b666017f1d69fb994674bb60eb7b3e
Raymond <ray@firestitch.com>
2017-01-25 18:30:46 -0500

    added new email validation http://emailregex.com/

--- dist/validate.js
+++ dist/validate.js
@@ -647,7 +647,7 @@
 	     * @returns {boolean} The result of the validation
 	     */
         function email(value) {
-			return !!fsUtil.string(value).match(/^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i);
+			return !!fsUtil.string(value).match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
         }
     });
 
